70abf439a6c026c7a83c6725a9238d026cf95289
Daniel Oxman <zeoxyman@gmail.com>
2018-01-03 15:01:13 -0500

    Forgot to build previously

--- build/index.js
+++ build/index.js
@@ -255,6 +255,8 @@ Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _react = __webpack_require__(4);
 
 var _react2 = _interopRequireDefault(_react);
@@ -269,13 +271,40 @@ function _interopRequireWildcard(obj) { if (obj && obj.__esModule) { return obj;
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function SlidePuzzle(props) {
-  return _react2.default.createElement(
-    'p',
-    null,
-    'Hello, world!'
-  );
-}
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+var SlidePuzzle = function (_Component) {
+  _inherits(SlidePuzzle, _Component);
+
+  function SlidePuzzle() {
+    _classCallCheck(this, SlidePuzzle);
+
+    return _possibleConstructorReturn(this, (SlidePuzzle.__proto__ || Object.getPrototypeOf(SlidePuzzle)).apply(this, arguments));
+  }
+
+  _createClass(SlidePuzzle, [{
+    key: 'render',
+    value: function render() {
+      _react2.default.createElement(
+        'div',
+        { 'class': 'slidepuzzle-board' },
+        Array.from(new Array(16), function (x, i) {
+          return i;
+        }).map(function (i) {
+          var squareID = shortid.generate();
+
+          return _react2.default.createElement('div', { key: squareID, 'class': 'slidepuzzle-piece' });
+        })
+      );
+    }
+  }]);
+
+  return SlidePuzzle;
+}(_react.Component);
 
 exports.default = SlidePuzzle;
 
@@ -288,7 +317,7 @@ exports = module.exports = __webpack_require__(7)(undefined);
 
 
 // module
-exports.push([module.i, "p {\n  color: red;\n}\n", ""]);
+exports.push([module.i, "html {\n  --board-size: 4;\n}\n\n.slidepuzzle-board {\n  width: 300px;\n  height: 300px;\n  display: grid;\n  grid-template-rows: repeat(var(--board-size), calc(300px / var(--board-size)));\n  grid-template-columns: repeat(var(--board-size), calc(300px / var(--board-size)));\n  grid-column-gap: 1px;\n  grid-row-gap: 1px;\n}\n\n.slidepuzzle-piece {\n  background-color: black;\n}\n", ""]);
 
 // exports
 
