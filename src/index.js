aafd429a0ce501441859968d47a3d64146f25f8b
Daniel Oxman <zeoxyman@gmail.com>
2018-01-02 23:53:55 -0500

    Initial test commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import * as shortid from 'shortid';
+import './index.css';
+
+
+function SlidePuzzle(props) {
+  return (
+    <p>Hello, world!</p>
+  );
+}
+
+export default SlidePuzzle;
